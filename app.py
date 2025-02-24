a13d34565948460b76127fc12f4a0240b03f0f35
praneswara <praneswara2003@gmail.com>
2025-02-24 14:21:34 +0530

    Updated code

--- app.py
+++ app.py
@@ -904,6 +904,6 @@ def get_file(filename):
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    ssocketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
 
 
